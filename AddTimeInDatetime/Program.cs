61303db2ec0b2777f49cb6772273534e2a5de855
Kamran Z <kamraneze@code.edu.az>
2023-08-25 11:35:42 +0400

    DateTime methods

---
+++ AddTimeInDatetime/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace AddTimeInDatetime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Enter date in this format: \n dd/MM/yyy hh:mm");
+            Console.ForegroundColor = ConsoleColor.White;
+            string? user = Console.ReadLine();
+            string dateFormat = "dd/MM/yyyy hh:mm";
+
+
+            var insert = DateTime.ParseExact(user, dateFormat, null);
+            insert = insert.AddDays(30);
+            insert = insert.AddHours(6);
+            insert = insert.AddMinutes(17);
+            Console.WriteLine(insert.ToString(dateFormat));
+
+
+        }
+    }
+}
